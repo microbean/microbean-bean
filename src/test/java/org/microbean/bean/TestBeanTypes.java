e4c373778d389d42952b19a9ea8d4b779cc1357b
Laird Nelson <ljnelson@gmail.com>
2025-01-05 16:05:03 -0800

    Road grading
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/test/java/org/microbean/bean/TestBeanTypes.java
+++ src/test/java/org/microbean/bean/TestBeanTypes.java
@@ -28,7 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// import static org.microbean.bean.BeanTypes.name;
+import static org.microbean.bean.Types.name;
 
 final class TestBeanTypes {
 
@@ -57,15 +57,15 @@ final class TestBeanTypes {
   final void testSupertypesOfint() {
     final List<? extends TypeMirror> ts = beanTypes.supertypes(domain.primitiveType(TypeKind.INT));
     assertEquals(1, ts.size());
-    assertEquals("int", beanTypes.name(ts.get(0)));
+    assertEquals("int", name(ts.get(0)));
   }
 
   @Test
   final void testBeanTypesOfint() {
     final List<? extends TypeMirror> ts = beanTypes.beanTypes(domain.primitiveType(TypeKind.INT));
     assertEquals(2, ts.size());
-    assertEquals("int", beanTypes.name(ts.get(0)));
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(1))); // per CDI specification
+    assertEquals("int", name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(1))); // per CDI specification
   }
 
 
@@ -73,26 +73,26 @@ final class TestBeanTypes {
   final void testDirectSupertypesOfObjectArrayAreASingleIntersectionType() {
     final List<? extends TypeMirror> ts = domain.directSupertypes(domain.arrayTypeOf(domain.declaredType("java.lang.Object")));
     assertEquals(1, ts.size());
-    assertEquals("java.io.Serializable&java.lang.Cloneable", beanTypes.name(ts.get(0))); // wut
+    assertEquals("java.io.Serializable&java.lang.Cloneable", name(ts.get(0))); // wut
   }
 
   @Test
   final void testSupertypesOfObjectArray() {
     final List<? extends TypeMirror> ts = beanTypes.supertypes(domain.arrayTypeOf(domain.declaredType("java.lang.Object")));
     assertEquals(5, ts.size());
-    assertEquals("java.lang.Object[]", beanTypes.name(ts.get(0)));
-    assertEquals("java.io.Serializable&java.lang.Cloneable", beanTypes.name(ts.get(1))); // wut
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(2)));
-    assertEquals("java.io.Serializable", beanTypes.name(ts.get(3)));
-    assertEquals("java.lang.Cloneable", beanTypes.name(ts.get(4)));
+    assertEquals("java.lang.Object[]", name(ts.get(0)));
+    assertEquals("java.io.Serializable&java.lang.Cloneable", name(ts.get(1))); // wut
+    assertEquals("java.lang.Object", name(ts.get(2)));
+    assertEquals("java.io.Serializable", name(ts.get(3)));
+    assertEquals("java.lang.Cloneable", name(ts.get(4)));
   }
 
   @Test
   final void testBeanTypesOfObjectArray() {
     final List<? extends TypeMirror> ts = beanTypes.beanTypes(domain.arrayTypeOf(domain.declaredType("java.lang.Object")));
     assertEquals(2, ts.size());
-    assertEquals("java.lang.Object[]", beanTypes.name(ts.get(0)));
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(1)));
+    assertEquals("java.lang.Object[]", name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(1)));
   }
 
 
@@ -100,26 +100,26 @@ final class TestBeanTypes {
   final void testSupertypesOfString() {
     final List<? extends TypeMirror> ts = beanTypes.supertypes(domain.declaredType("java.lang.String"));
     assertEquals(7, ts.size(), ts.toString());
-    assertEquals("java.lang.String", beanTypes.name(ts.get(0)));
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(1)));
-    assertEquals("java.io.Serializable", beanTypes.name(ts.get(2)));
-    assertEquals("java.lang.CharSequence", beanTypes.name(ts.get(3)));
-    assertEquals("java.lang.Comparable", beanTypes.name(ts.get(4)));
-    assertEquals("java.lang.constant.Constable", beanTypes.name(ts.get(5)));
-    assertEquals("java.lang.constant.ConstantDesc", beanTypes.name(ts.get(6)));
+    assertEquals("java.lang.String", name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(1)));
+    assertEquals("java.io.Serializable", name(ts.get(2)));
+    assertEquals("java.lang.CharSequence", name(ts.get(3)));
+    assertEquals("java.lang.Comparable", name(ts.get(4)));
+    assertEquals("java.lang.constant.Constable", name(ts.get(5)));
+    assertEquals("java.lang.constant.ConstantDesc", name(ts.get(6)));
   }
 
   @Test
   final void testBeanTypesOfString() {
     final List<? extends TypeMirror> ts = beanTypes.beanTypes(domain.declaredType("java.lang.String"));
     assertEquals(7, ts.size(), ts.toString());
-    assertEquals("java.lang.String", beanTypes.name(ts.get(0)));
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(1)));
-    assertEquals("java.io.Serializable", beanTypes.name(ts.get(2)));
-    assertEquals("java.lang.CharSequence", beanTypes.name(ts.get(3)));
-    assertEquals("java.lang.Comparable", beanTypes.name(ts.get(4)));
-    assertEquals("java.lang.constant.Constable", beanTypes.name(ts.get(5)));
-    assertEquals("java.lang.constant.ConstantDesc", beanTypes.name(ts.get(6)));
+    assertEquals("java.lang.String", name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(1)));
+    assertEquals("java.io.Serializable", name(ts.get(2)));
+    assertEquals("java.lang.CharSequence", name(ts.get(3)));
+    assertEquals("java.lang.Comparable", name(ts.get(4)));
+    assertEquals("java.lang.constant.Constable", name(ts.get(5)));
+    assertEquals("java.lang.constant.ConstantDesc", name(ts.get(6)));
 
   }
 
@@ -135,17 +135,17 @@ final class TestBeanTypes {
   final void testDirectSupertypesOfInterfaceBExtendsA() {
     final List<? extends TypeMirror> ts = domain.directSupertypes(domain.declaredType("org.microbean.bean.TestBeanTypes.B"));
     assertEquals(2, ts.size());
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(0)));
-    assertEquals("org.microbean.bean.TestBeanTypes.A", beanTypes.name(ts.get(1)));
+    assertEquals("java.lang.Object", name(ts.get(0)));
+    assertEquals("org.microbean.bean.TestBeanTypes.A", name(ts.get(1)));
   }
 
   @Test
   final void testDirectSupertypesOfInterfaceCExtendsBAreInDeclarationOrder() {
     final List<? extends TypeMirror> ts = domain.directSupertypes(domain.declaredType("org.microbean.bean.TestBeanTypes.C"));
     assertEquals(3, ts.size());
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(0)));
-    assertEquals("org.microbean.bean.TestBeanTypes.A", beanTypes.name(ts.get(1)));
-    assertEquals("org.microbean.bean.TestBeanTypes.B", beanTypes.name(ts.get(2)));
+    assertEquals("java.lang.Object", name(ts.get(0)));
+    assertEquals("org.microbean.bean.TestBeanTypes.A", name(ts.get(1)));
+    assertEquals("org.microbean.bean.TestBeanTypes.B", name(ts.get(2)));
   }
 
 
@@ -156,11 +156,11 @@ final class TestBeanTypes {
                                             domain.wildcardType(domain.declaredType("java.lang.String"),
                                                              null)));
     assertEquals(5, ts.size());
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(0)));
-    assertEquals("java.util.List", beanTypes.name(ts.get(1)));
-    assertEquals("java.util.SequencedCollection", beanTypes.name(ts.get(2)));
-    assertEquals("java.util.Collection", beanTypes.name(ts.get(3)));
-    assertEquals("java.lang.Iterable", beanTypes.name(ts.get(4)));
+    assertEquals("java.lang.Object", name(ts.get(0)));
+    assertEquals("java.util.List", name(ts.get(1)));
+    assertEquals("java.util.SequencedCollection", name(ts.get(2)));
+    assertEquals("java.util.Collection", name(ts.get(3)));
+    assertEquals("java.lang.Iterable", name(ts.get(4)));
   }
 
   @Test
@@ -169,7 +169,7 @@ final class TestBeanTypes {
                                                                      domain.wildcardType(domain.declaredType("java.lang.String"),
                                                                                       null)));
     assertEquals(1, ts.size());
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(0)));
   }
 
 
@@ -193,15 +193,15 @@ final class TestBeanTypes {
     final List<TypeMirror> ts =
       beanTypes.supertypes(domain.typeVariable(this.getClass().getDeclaredMethod("testSupertypesOfTypeVariable").getTypeParameters()[0]));
     assertEquals(2, ts.size());
-    assertEquals("T", beanTypes.name(ts.get(0)));
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(1)));
+    assertEquals("T", name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(1)));
   }
 
   @Test
   final <T> void testBeanTypesOfTypeVariable() throws ReflectiveOperationException {
     final List<TypeMirror> ts = beanTypes.beanTypes(domain.typeVariable(this.getClass().getDeclaredMethod("testBeanTypesOfTypeVariable").getTypeParameters()[0]));
     assertEquals(1, ts.size());
-    assertEquals("java.lang.Object", beanTypes.name(ts.get(0)));
+    assertEquals("java.lang.Object", name(ts.get(0)));
   }
   */
 
